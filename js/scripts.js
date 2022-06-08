d07e2f6edb1361508d711f218e042e6adcf09dd9
Liam Campbell <liam22campbell@gmail.com>
2022-06-08 08:14:32 -0700

    BUGFIX edit js to make the hide function, and append methods work.

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,11 @@
 $(document).ready(function() {
 	$("form#transportation_survey").submit(function(event) {
 		event.preventDefault();
-		$("#work-repsonses").show();
-		$("input:checkbox[name=work-transportation:checked").each(function() {
+		$("#work-responses").show();
+		$("input:checkbox[name=work-transportation]:checked").each(function() {
 				const workTransportationMode = $(this).val();
-				$("work-responses").append(workTransportationMode + "<br>");
+				$("#work-responses").append(workTransportationMode + "<br>");
 		});
-		$('transportation_survey').hide();
+		$('#transportation_survey').hide();
 	});
 });
