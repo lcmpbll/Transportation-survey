55a778fdfb7197fa5debb3db21e109743b34dd72
Liam Campbell <liam22campbell@gmail.com>
2022-06-09 16:22:10 -0700

    BUGFIX fun-responses should now Append.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,11 @@ $(document).ready(function() {
 	$("form#transportation_survey").submit(function(event) {
 		event.preventDefault();
 		$("#work-responses").show();
+		$("#fun-responses").show();
+		$("input:checkbox[name=fun-transportation]:checked").each(function(){
+			const workTransportationMode = $(this).val();
+			$("#fun-responses").append(funTransportationMode + "<br>");
+		});
 		$("input:checkbox[name=work-transportation]:checked").each(function() {
 				const workTransportationMode = $(this).val();
 				$("#work-responses").append(workTransportationMode + "<br>");
